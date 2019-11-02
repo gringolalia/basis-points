276e12e5d79534320fef7424b3493fb42e326d62
Colin Brayton <gringolalia@gmail.com>
2019-11-02 12:59:42 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+// import Basis from 'getbasis/src/js/basis'
+// import 'getbasis/src/css/basis
+
+// Vue.use(Basis)'
 
 Vue.config.productionTip = false
 
