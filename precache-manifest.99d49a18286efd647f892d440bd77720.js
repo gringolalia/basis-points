a88ab469a147fe5ef76cd536ca6756ddce4a61f8
Colin Brayton <gringolalia@gmail.com>
2019-11-02 13:03:20 -0300

    deploy

---
+++ precache-manifest.99d49a18286efd647f892d440bd77720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "68642cd572f7534bfe3f",
+    "url": "/basis-points/css/app.329c7657.css"
+  },
+  {
+    "revision": "68642cd572f7534bfe3f",
+    "url": "/basis-points/js/app.6a59b4bc.js"
+  },
+  {
+    "revision": "38c62dc59b8d9e5c4455",
+    "url": "/basis-points/js/chunk-vendors.c50760ac.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/basis-points/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d97e6b9573bcdf16d7d915dcfab5cb27",
+    "url": "/basis-points/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/basis-points/robots.txt"
+  }
+];
